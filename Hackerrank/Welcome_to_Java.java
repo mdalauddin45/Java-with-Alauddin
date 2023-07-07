315c7254c150832a117a1dd9ce16cb3c5181ff08
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2023-07-07 21:50:31 +0600

    java problem solved

---
+++ Hackerrank/Welcome_to_Java.java
@@ -0,0 +1,10 @@
+/**
+ * Welcome_to_Java
+ */
+public class Welcome_to_Java {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World.");
+        System.out.println("Hello, Java.");
+    }
+}
