65e567b4bda1c4182245cfc036a047a79b82a9ec
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2023-07-14 00:30:52 +0600

    solved

---
+++ Basic-I/Product_two_nums.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Product_two_nums {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Input first number: ");
+        int x=sc.nextInt();
+        System.out.print("Input second  number: ");
+        int y=sc.nextInt();
+        int r=x*y;
+        System.out.println(x+" x "+y+" = "+r);
+    }
+}
