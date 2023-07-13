dd9325208466e03a01474abab33d9cc4c6927093
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2023-07-14 00:16:34 +0600

    String input output

---
+++ Basic-I/Hello.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Hello{
+    public static void main(String[] args){
+        Scanner sc= new Scanner(System.in);
+        String str = sc.nextLine();
+        String str1 = sc.nextLine();
+        System.out.println(str);
+        System.out.println(str1);
+    }
+}
