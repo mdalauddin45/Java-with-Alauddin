1d881c9f4428037541fde5fece587cd415cf34f0
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2023-07-14 00:19:09 +0600

    Sum of two int

---
+++ Basic-I/Sum_of_two.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Sum_of_two {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        int x=sc.nextInt();
+        int y=sc.nextInt();
+        int r=x+y;
+        System.out.println(r);
+    }
+}
