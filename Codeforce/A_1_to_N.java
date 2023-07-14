60c19a2ac2ec9238af0c50327bf74129c869c257
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2023-07-14 14:31:16 +0600

    1 to N solved

---
+++ Codeforce/A_1_to_N.java
@@ -0,0 +1,15 @@
+/**
+ * A_1_to_N
+ */
+import java.util.Scanner;
+public class A_1_to_N {
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int x=sc.nextInt();
+        for(int i=1;i<=x;i++)
+        {
+            System.out.println(i);
+        }
+    }
+}
