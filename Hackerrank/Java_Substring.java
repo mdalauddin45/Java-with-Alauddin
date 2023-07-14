1483d0be5db58f33e1f7e1d727ae28befe71a735
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2023-07-14 23:51:29 +0600

    Java_Substring solved

---
+++ Hackerrank/Java_Substring.java
@@ -0,0 +1,14 @@
+/**
+ * Java_Substring
+ */
+import java.util.Scanner;
+public class Java_Substring {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String S = in.next();
+        int start = in.nextInt();
+        int end = in.nextInt();
+        System.out.println(S.substring(start,end));
+    }
+}
