b63a76d77b7af48ee1f42fa6a940d5eec7da0d80
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2023-07-14 23:56:22 +0600

    Java_Substring_Comparisons solved

---
+++ Hackerrank/Java_Substring_Comparisons.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+/**
+ * Java_Substring_Comparisons
+ */
+public class Java_Substring_Comparisons {
+    public static String getSmallestAndLargest(String s, int k) {
+        String smallest = "";
+        String largest = "";
+        
+        for(int i=0;i<=s.length()-k;i++){
+            String sub = s.substring(i,i+k);
+            if(i==0){
+                smallest = sub;
+            }
+            if(sub.compareTo(largest)>0){
+                largest = sub;
+            }else if(sub.compareTo(smallest)<0){
+                smallest = sub;
+            }
+        }
+        return smallest + "\n" + largest;
+    }
+
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String s = scan.next();
+        int k = scan.nextInt();
+        scan.close();
+      
+        System.out.println(getSmallestAndLargest(s, k));
+    }
+}
