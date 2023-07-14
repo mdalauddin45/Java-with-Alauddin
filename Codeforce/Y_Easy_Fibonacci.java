dfb33ad0fd06959f90d643a7419cc416cef41b6d
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2023-07-14 15:49:53 +0600

    solved case error

--- Codeforce/Y_Easy_Fibonacci.java
+++ Codeforce/Y_Easy_Fibonacci.java
@@ -3,16 +3,17 @@
  */
 import java.util.Scanner;
 public class Y_Easy_Fibonacci {
-    public static int fibonacci(int n)
-    {
-        if(n<=1) return n;
-        return fibonacci(n-1)+fibonacci(n-2);
-    }
+   
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int x = sc.nextInt();
-          for (int i = 0; i < x; i++) {
-            System.out.print(fibonacci(i) + " ");
+        int first = 0;
+        int second = 1;
+        for (int i = 0; i < x; i++) {
+            System.out.print(first + " ");
+            int next = first + second;
+            first = second;
+            second = next;
         }
     }
 }
