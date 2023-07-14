182197141fcbd59adac7da6b2a69347440d92335
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2023-07-14 14:19:54 +0600

    pattern

---
+++ Basic-I/Pattern.java
@@ -0,0 +1,8 @@
+public class Pattern {
+    public static void main(String[] args) {
+        System.out.println("  J    a   v     v  a                                                  \r\n" + //
+                "   J   a a   v   v  a a                                                 \r\n" + //
+                "J  J  aaaaa   V V  aaaaa                                                \r\n" + //
+                " JJ  a     a   V  a     a");
+    }
+}
