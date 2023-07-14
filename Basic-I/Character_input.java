340cefac263190a6aa4448d64a1b36cae13d9121
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2023-07-14 14:13:11 +0600

    one char input

---
+++ Basic-I/Character_input.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Character_input {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        //one char input
+        char ch = sc.next().charAt(0);
+        System.out.println(ch);
+
+    }
+}
--- Basic-I/Product_two_nums.java
+++ Basic-I/Product_two_nums.java
@@ -2,9 +2,9 @@ import java.util.Scanner;
 public class Product_two_nums {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
-        System.out.print("Input first number: ");
+        // System.out.print("Input first number: ");
         int x=sc.nextInt();
-        System.out.print("Input second  number: ");
+        // System.out.print("Input second  number: ");
         int y=sc.nextInt();
         int r=x*y;
         System.out.println(x+" x "+y+" = "+r);
