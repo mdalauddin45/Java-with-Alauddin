e9ffb81c0baacb11eb777790dfcce7a93c61b2bb
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2023-07-15 12:12:28 +0600

    Car project

---
+++ University/Car.java
@@ -0,0 +1,14 @@
+public class Car {
+    String make = "Chevrolet";
+    String model = "Corvette";
+    int year = 2023;
+    String color = "Blue";
+    double price = 50000.00;
+    
+    void drive (){
+        System.out.println("You drive the car");
+    }
+    void brake(){
+        System.out.println("You step on the brakes");
+    }
+}
---
+++ University/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        Car myCar = new Car();
+       // System.out.println(myCar.make);
+       // System.out.println(myCar.model);
+
+        //myCar.drive();
+        myCar.brake();
+    }
+}
