ac593aaa644c447297f96fba112f01391fd65e1d
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2023-07-15 11:46:38 +0600

    String all methode

---
+++ University/String_mathode.java
@@ -0,0 +1,21 @@
+public class String_mathode {
+    public static void main(String[] args) {
+        String name = "  Alauddin  ";
+        String nm = "khan";
+        // boolean result =  name.equals("name"); //output false;
+        // int result = name.length();
+        // char result = name.charAt(0);//A
+        // int result = name.indexOf("i"); // position of a string 
+        // boolean result = name.isEmpty();//false
+        // boolean result = name.isBlank();//false
+        // String result = name.toUpperCase();
+        // String result = name.toLowerCase();
+        // String result = name.substring(0, 3);//Ala
+        // String result = name.substring(0, 3).toUpperCase();//ALA
+        // String result = name.trim();
+        // String result = name.replace(name, nm); //khan
+
+        // System.out.println(result); 
+
+    }
+}
