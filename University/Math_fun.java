eb9045ad889fc410c670ac227cd0fda3dd02c9c4
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2023-07-21 19:10:23 +0600

    Math funtion

---
+++ University/Math_fun.java
@@ -0,0 +1,16 @@
+public class Math_fun {
+    public static void main(String[] args) {
+        double x = -3.14;
+        double y = 10;
+        // double z = Math.min(x, y); // return min value is 3.14
+        // double z = Math.max(x, y); // return max value is 10
+        int a = 16;
+        int b=2;
+        // double z = Math.abs(a); // return a positive value 
+        // double z = Math.sqrt(a); // return borgomul
+        double z = Math.pow(a,b); 
+
+
+        System.out.println(z);
+    }
+}
