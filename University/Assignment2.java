3e61e611662e2c3da750cad6f3cf4b3ab5169187
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2023-07-21 20:00:56 +0600

    Assignment2 anisul sir

---
+++ University/Assignment2.java
@@ -0,0 +1,23 @@
+public class Assignment2 {
+    public static void main(String[] args) {
+        int id;
+        String title;
+        int price;
+        String description;
+        String category;
+
+        // assign
+        id = 101;
+        title = "iphone15";
+        price = 1895;
+        description = "perfect product with best image quality";
+        category = "phone";
+
+        // print
+        System.out.println("id: " + id);
+        System.out.println("title: " + title);
+        System.out.println("price: " + price);
+        System.out.println("description: " + description);
+        System.out.println("category: " + category);
+    }
+}
