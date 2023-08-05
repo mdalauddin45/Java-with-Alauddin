2cacd192cc37de6a11dcb10226cca76f7b81a662
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2023-08-05 14:39:21 +0600

    Day_4_Class_vs_Instance solved

--- Hacker Rank 30 Days/Day_4_Class_vs_Instance.java
+++ Hacker Rank 30 Days/Day_4_Class_vs_Instance.java
@@ -7,22 +7,14 @@ public class Day_4_Class_vs_Instance {
     private int age;
 
     public Day_4_Class_vs_Instance(int i) {
-        if (i < 0) {
-            System.out.println("Age is not valid, setting age to 0.");
-            age = 0;
-        } else {
-            age = i;
-        }
+        if(i<0)System.out.println("Age is not valid, setting age to 0.");
+        else age = i;
     }
 
     public void amIOld() {
-        if (age < 13) {
-            System.out.println("You are young.");
-        } else if (age >= 13 && age < 18) {
-            System.out.println("You are a teenager.");
-        } else {
-            System.out.println("You are old.");
-        }
+        if(age<13)System.out.println("You are young.");
+        else if(age>=13 && age<18)System.out.println("You are a teenager.");
+        else System.out.println("You are old.");
     }
 
     public void yearPasses() {
