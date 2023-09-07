94e0de911b2708cc0fd98074336e746a7e694fb7
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2023-09-07 22:15:29 +0600

    Hierarchical solved

---
+++ University/Mid Exam/Hierarchical_inheritance.java
@@ -0,0 +1,16 @@
+class Employee{
+    float salary=40000;
+}
+class Programmer extends Employee{
+    int bonus = 1000;
+}
+class Overtimes extends Employee{
+    int over=800;
+}
+public class Hierarchical_inheritance {
+    public static void main(String[] args) {
+       Programmer p=new Programmer();
+       Overtimes o=new Overtimes();
+       System.out.println("Total beton "+(p.salary+p.bonus+o.over));
+    }
+}
