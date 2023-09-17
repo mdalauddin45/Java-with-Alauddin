9ec770e6c9d2d37559dd66b12190d4d35cf75ba8
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2023-09-18 01:18:56 +0600

    string

---
+++ University/Final/String_methodes.java
@@ -0,0 +1,28 @@
+/**
+ * String_methodes
+ */
+public class String_methodes {
+
+    public static void main(String[] args) {
+        // String s = "java"; //literal 
+        // String s1 = new String("kava"); // by new keyword
+        // char ch[]={'a','b','c'};
+        // System.out.println(ch.length); // lenght
+        // System.out.println(s.charAt(0)); // charAt(index) find index 
+        // System.out.println(s.substring(0, 2)); // substring
+        // System.out.println(s.substring(1)); // substring
+        // String name="what do you know about me";  
+        // System.out.println(name.contains("do you know"));  
+        // System.out.println(name.contains("about"));  
+        // System.out.println(name.contains("hello"));  
+
+        // String joinString1=String.join(" ","welcome","to","javatpoint");  
+        // System.out.println(joinString1);  
+        // String s1="java string split method by javatpoint";  
+        // String[] words=s1.split(" ");
+        // for(String w:words){  
+        //     System.out.println(w);  
+        // } 
+
+    }
+}
