bb406d01b0db8c0bdb6e7e1460f15b19d4264c9e
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2023-09-18 20:18:17 +0600

    String_method

---
+++ University/Mid Exam/String_method.java
@@ -0,0 +1,10 @@
+public class String_method {
+    public static void main(String[] args) {
+        char[] ch ={'a','l','a',' ','u','d','d','i','n'};
+        // String s= new String(ch);  
+        // System.out.println(s);
+        for(char x:ch){
+            System.out.print(x);
+        }
+    }
+}
