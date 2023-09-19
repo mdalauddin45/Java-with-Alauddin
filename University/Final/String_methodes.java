685e3502b4be299d67f84565dc820f2fbcdd4efb
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2023-09-19 08:57:23 +0600

    string method

--- University/Final/String_methodes.java
+++ University/Final/String_methodes.java
@@ -4,17 +4,28 @@
 public class String_methodes {
 
     public static void main(String[] args) {
-        // String s = "java"; //literal 
+        String s = "java course"; //literal 
         // String s1 = new String("kava"); // by new keyword
-        // char ch[]={'a','b','c'};
+        char ch[]={'a','b','c'};
+        // System.out.println(s.length()); // lenght
         // System.out.println(ch.length); // lenght
-        // System.out.println(s.charAt(0)); // charAt(index) find index 
+        // System.out.println(s.charAt(2)); // charAt(index) find index 
         // System.out.println(s.substring(0, 2)); // substring
         // System.out.println(s.substring(1)); // substring
         // String name="what do you know about me";  
         // System.out.println(name.contains("do you know"));  
         // System.out.println(name.contains("about"));  
-        // System.out.println(name.contains("hello"));  
+        // System.out.println(name.contains("hello")); 
+        // System.out.println(s.concat(" course"));
+        // System.out.println(s.contains("j")); 
+        // System.out.println(s.endsWith("a")); 
+        // System.out.format("%10s", "css");
+        // String[] w = s.split("a");
+        // for(String x:w){
+        //     System.out.println(x);
+        // }
+        // System.out.println(s.trim());
+        System.out.println(s.valueOf(ch));
 
         // String joinString1=String.join(" ","welcome","to","javatpoint");  
         // System.out.println(joinString1);  
