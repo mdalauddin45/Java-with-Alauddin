c3ba926d1e1ecedb3b7cfcd89495b038a65ad258
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2023-11-22 02:01:53 +0600

    file writer

--- University/CT/file_writes.java
+++ University/CT/file_writes.java
@@ -5,7 +5,8 @@ public class file_writes {
        try {
          FileWriter fileWriter = new FileWriter("poem.txt");
          fileWriter.write("Asad sir roks\n");
-         fileWriter.append("He is the Java Teacher"); // jode aki file a line ata likty chaw taholy append korba for example toi jode cho soudi arab jai taka r nari 2 ta taily append shika lagbbo
+         // jode aki file a line ata likty chaw taholy append korba
+         fileWriter.append("He is the Java Teacher");
          fileWriter.close();
        } catch (Exception e) {
         System.out.println(e.getMessage());
