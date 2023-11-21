3dc213edb7bdfd4405f32d1adf954555d1e7c8ef
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2023-11-22 02:48:20 +0600

    swing

---
+++ University/CT/SwingExample.java
@@ -0,0 +1,23 @@
+import javax.swing.*;
+
+public class SwingExample {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Swing Components Example");
+        JPanel panel = new JPanel();
+
+        JLabel label = new JLabel("Enter your name:");
+        JTextField textField = new JTextField(15);
+        JButton button = new JButton("Submit");
+
+        panel.add(label);
+        panel.add(textField);
+        panel.add(button);
+
+        frame.add(panel);
+        frame.setSize(300, 150);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+
+        frame.setLocationRelativeTo(null); 
+    }
+}
