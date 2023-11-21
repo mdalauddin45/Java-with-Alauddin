2c3e2300e4aea24e77f718aad057aa0c46694c93
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2023-11-22 00:53:39 +0600

    file done

--- University/CT/file_obj.java
+++ University/CT/file_obj.java
@@ -7,6 +7,11 @@ public class file_obj {
         File file = new File("message.txt");
         if(file.exists()) {
             System.out.println("This file exists");
+            //if you need 
+            System.out.println(file.getPath()); // return file name 
+            System.out.println(file.isFile()); // return true or false
+            System.out.println(file.getAbsolutePath()); // return file location 
+            file.delete(); // ata use kory file delete kora hoi
         }else{
             System.out.println("This file does not exist");
         }
