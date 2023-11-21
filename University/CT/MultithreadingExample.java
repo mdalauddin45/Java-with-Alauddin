e51460df5dc04c3ab0cecd7f0070b28241b45ced
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2023-11-22 02:30:57 +0600

    MultithreadingExample

---
+++ University/CT/MultithreadingExample.java
@@ -0,0 +1,25 @@
+class MyTask implements Runnable{
+    String name;
+    public MyTask(String name){
+        this.name = name;
+    }
+    public void run(){
+        System.out.println(Thread.currentThread().getName() + " " + name);
+    }
+}
+public class MultithreadingExample {
+
+    public static void main(String[] args) {
+        MyTask task1 = new MyTask("test-1");
+        MyTask task2 = new MyTask("test-2");
+        MyTask task3 = new MyTask("test-3");
+
+        Thread thread1 = new Thread(task1);
+        Thread thread2 = new Thread(task2);
+        Thread thread3 = new Thread(task3);
+        
+        thread1.start();
+        thread2.start();
+        thread3.start();
+    }
+}
--- University/CT/Thread_Example.java
+++ University/CT/Thread_Example.java
@@ -1,13 +1,6 @@
 class MyThread extends Thread {
     public void run() {
-        for (int i = 0; i <= 5; i++) {
-            System.out.println(Thread.currentThread().getName() + " Count " + i);
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException e) {
-                System.out.println(e.getMessage());
-            }
-        }
+        System.out.println(Thread.currentThread().getName());
     }
 }
 public class Thread_Example {
