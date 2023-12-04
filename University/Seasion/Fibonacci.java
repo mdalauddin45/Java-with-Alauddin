2f1a11fcfe5283333933c0c5c0460ca6d9cd277e
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2023-12-05 01:43:53 +0600

    fibonacci

---
+++ University/Seasion/Fibonacci.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Fibonacci {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        for (int i = 0; i < x; i++) {
+            System.out.print(calculateFibonacci(i) + " ");
+        }
+    }
+
+    public static int calculateFibonacci(int n) {
+        if (n <= 1) {
+            return n;
+        }
+        return calculateFibonacci(n - 1) + calculateFibonacci(n - 2);
+    }
+}
